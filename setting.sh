2ccae18375e6261d8e43b776718507e7c1bcfc41
KevinTheRainmaker <sweetrainforyou@gmail.com>
2022-12-13 11:24:24 +0000

    build(setting.sh): A100 setting

---
+++ setting.sh
@@ -0,0 +1,3 @@
+export TORCH_CUDA_ARCH_LIST=8.0
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install albumentations split-image
