ad03d842a65ed4d09f1968017b3ec81825cbb472
Jeongwook-Lee <dlwjddnr98@gmail.com>
2022-12-13 11:38:10 +0000

    feat(crop_4by4):crop SPN7_revise

--- dataset/crop_4by4.py
+++ dataset/crop_4by4.py
@@ -9,9 +9,11 @@ def crop_fbf(data_path: str, mode: str):
     dir_output_image = join(data_path, mode, "images_256")
     dir_output_label = join(data_path, mode, "labels_256")
     for (root, directories, files) in os.walk(dir_path_image):
-        os.system(f'split-image {root} 4 4 --output-dir ./SPN7/{mode}/images_256')
+        image_dir_name = os.path.basename(os.path.normpath(root))
+        os.system(f'split-image {root} 4 4 --output-dir ./SPN7/{mode}/images_256/{image_dir_name}')
     for (root, directories, files) in os.walk(dir_path_label):
-        os.system(f'split-image {root} 4 4 --output-dir ./SPN7/{mode}/labels_256')
+        image_dir_name = os.path.basename(os.path.normpath(root))
+        os.system(f'split-image {root} 4 4 --output-dir ./SPN7/{mode}/labels_256/{image_dir_name}')
 
 
 if __name__ == '__main__':
