3ebba0b948a07b6fe8a2379169a12b5d384ce2db
KevinTheRainmaker <sweetrainforyou@gmail.com>
2022-12-13 12:56:50 +0000

    perf(crop_4by4.py): data processed path

--- dataset/crop_4by4.py
+++ dataset/crop_4by4.py
@@ -10,10 +10,10 @@ def crop_fbf(data_path: str, mode: str):
     dir_output_label = join(data_path, mode, "labels_256")
     for (root, directories, files) in os.walk(dir_path_image):
         image_dir_name = os.path.basename(os.path.normpath(root))
-        os.system(f'split-image {root} 4 4 --output-dir ./SPN7/{mode}/images_256/{image_dir_name}')
+        os.system(f'split-image {root} 4 4 --output-dir ./SPN7_256/{mode}/images_256/{image_dir_name}')
     for (root, directories, files) in os.walk(dir_path_label):
         image_dir_name = os.path.basename(os.path.normpath(root))
-        os.system(f'split-image {root} 4 4 --output-dir ./SPN7/{mode}/labels_256/{image_dir_name}')
+        os.system(f'split-image {root} 4 4 --output-dir ./SPN7_256/{mode}/labels_256/{image_dir_name}')
 
 
 if __name__ == '__main__':
