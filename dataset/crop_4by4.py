aa57a2011b5fd309850ba4f44c6c455f08d2e2e7
KevinTheRainmaker <sweetrainforyou@gmail.com>
2022-12-14 07:26:35 +0000

    refactor(crop_4by4.py): delete dir_output_path

--- dataset/crop_4by4.py
+++ dataset/crop_4by4.py
@@ -6,8 +6,6 @@ from split_image import split_image
 def crop_fbf(data_path: str, mode: str):
     dir_path_image = join(data_path, mode, "images")
     dir_path_label = join(data_path, mode, "labels")
-    dir_output_image = join(data_path, mode, "images_256")
-    dir_output_label = join(data_path, mode, "labels_256")
     for (root, directories, files) in os.walk(dir_path_image):
         image_dir_name = os.path.basename(os.path.normpath(root))
         os.system(f'split-image {root} 4 4 --output-dir ./SPN7_256/{mode}/images_256/{image_dir_name}')
